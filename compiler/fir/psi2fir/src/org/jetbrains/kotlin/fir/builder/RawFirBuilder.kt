a6d50f51d9cdbf526b975138f0e27b01d9528f5e
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-03-28 19:11:02 +0300

    Raw FIR: make object/sealed/enum constructor visibility private

--- compiler/fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
+++ compiler/fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
@@ -383,11 +383,18 @@ class RawFirBuilder(val session: FirSession, val stubMode: Boolean) {
                     superTypeCallEntry?.extractArgumentsTo(this)
                 }
             }
+
+            fun defaultVisibility() =
+                if (owner is KtObjectDeclaration || owner.hasModifier(SEALED_KEYWORD) || owner.hasModifier(ENUM_KEYWORD))
+                    Visibilities.PRIVATE
+                else
+                    Visibilities.UNKNOWN
+
             val firConstructor = FirPrimaryConstructorImpl(
                 session,
                 this ?: owner,
                 FirFunctionSymbol(callableIdForClassConstructor()),
-                this?.visibility ?: Visibilities.UNKNOWN,
+                this?.visibility ?: defaultVisibility(),
                 this?.hasExpectModifier() ?: false,
                 this?.hasActualModifier() ?: false,
                 delegatedSelfTypeRef,
