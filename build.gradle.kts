b7acfed27041f8f0c1d771fcd7b2de839181f9c8
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2019-05-03 17:20:49 +0200

    Add missing scripting tests to the global task, make them parallel

--- build.gradle.kts
+++ build.gradle.kts
@@ -527,6 +527,8 @@ tasks {
         dependsOn("dist")
         dependsOn(":kotlin-script-util:test")
         dependsOn(":kotlin-scripting-jvm-host:test")
+        dependsOn(":kotlin-scripting-jsr223:test")
+        dependsOn(":kotlin-main-kts-test:test")
     }
 
     create("compilerTest") {
