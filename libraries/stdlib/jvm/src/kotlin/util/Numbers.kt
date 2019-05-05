5cf703e48d839bc1ab09cc332e3c8aae0f37ed8f
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-05-05 06:15:00 +0300

    JVM implementations, docs and tests for bit query functions
    #KT-12749

--- libraries/stdlib/jvm/src/kotlin/util/Numbers.kt
+++ libraries/stdlib/jvm/src/kotlin/util/Numbers.kt
@@ -92,3 +92,178 @@ public actual inline fun Float.toRawBits(): Int = java.lang.Float.floatToRawIntB
 @SinceKotlin("1.2")
 @kotlin.internal.InlineOnly
 public actual inline fun Float.Companion.fromBits(bits: Int): Float = java.lang.Float.intBitsToFloat(bits)
+
+
+/**
+ * Counts the number of set bits in the binary representation of this [Int] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Int.countOneBits(): Int = Integer.bitCount(this)
+
+/**
+ * Counts the number of consecutive most significant bits that are zero in the binary representation of this [Int] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Int.countLeadingZeroBits(): Int = Integer.numberOfLeadingZeros(this)
+
+/**
+ * Counts the number of consecutive least significant bits that are zero in the binary representation of this [Int] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Int.countTrailingZeroBits(): Int = Integer.numberOfTrailingZeros(this)
+
+/**
+ * Returns a number having a single bit set in the position of the most significant set bit of this [Int] number,
+ * or zero, if this number is zero.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Int.takeHighestOneBit(): Int = Integer.highestOneBit(this)
+
+/**
+ * Returns a number having a single bit set in the position of the least significant set bit of this [Int] number,
+ * or zero, if this number is zero.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Int.takeLowestOneBit(): Int = Integer.lowestOneBit(this)
+
+
+
+
+/**
+ * Counts the number of set bits in the binary representation of this [Long] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Long.countOneBits(): Int = java.lang.Long.bitCount(this)
+
+/**
+ * Counts the number of consecutive most significant bits that are zero in the binary representation of this [Long] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Long.countLeadingZeroBits(): Int = java.lang.Long.numberOfLeadingZeros(this)
+
+/**
+ * Counts the number of consecutive least significant bits that are zero in the binary representation of this [Long] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Long.countTrailingZeroBits(): Int = java.lang.Long.numberOfTrailingZeros(this)
+
+/**
+ * Returns a number having a single bit set in the position of the most significant set bit of this [Long] number,
+ * or zero, if this number is zero.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Long.takeHighestOneBit(): Long = java.lang.Long.highestOneBit(this)
+
+/**
+ * Returns a number having a single bit set in the position of the least significant set bit of this [Long] number,
+ * or zero, if this number is zero.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Long.takeLowestOneBit(): Long = java.lang.Long.lowestOneBit(this)
+
+
+/**
+ * Counts the number of set bits in the binary representation of this [Byte] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Byte.countOneBits(): Int = (toInt() and 0xFF).countOneBits()
+
+/**
+ * Counts the number of consecutive most significant bits that are zero in the binary representation of this [Byte] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Byte.countLeadingZeroBits(): Int = (toInt() and 0xFF).countLeadingZeroBits() - (Int.SIZE_BITS - Byte.SIZE_BITS)
+
+/**
+ * Counts the number of consecutive least significant bits that are zero in the binary representation of this [Byte] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Byte.countTrailingZeroBits(): Int = (toInt() or 0x100).countTrailingZeroBits()
+
+/**
+ * Returns a number having a single bit set in the position of the most significant set bit of this [Byte] number,
+ * or zero, if this number is zero.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Byte.takeHighestOneBit(): Byte = (toInt() and 0xFF).takeHighestOneBit().toByte()
+
+/**
+ * Returns a number having a single bit set in the position of the least significant set bit of this [Byte] number,
+ * or zero, if this number is zero.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Byte.takeLowestOneBit(): Byte = toInt().takeLowestOneBit().toByte()
+
+
+
+/**
+ * Counts the number of set bits in the binary representation of this [Short] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Short.countOneBits(): Int = (toInt() and 0xFFFF).countOneBits()
+
+/**
+ * Counts the number of consecutive most significant bits that are zero in the binary representation of this [Short] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Short.countLeadingZeroBits(): Int = (toInt() and 0xFFFF).countLeadingZeroBits() - (Int.SIZE_BITS - Short.SIZE_BITS)
+
+/**
+ * Counts the number of consecutive least significant bits that are zero in the binary representation of this [Short] number.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Short.countTrailingZeroBits(): Int = (toInt() or 0x10000).countTrailingZeroBits()
+
+/**
+ * Returns a number having a single bit set in the position of the most significant set bit of this [Short] number,
+ * or zero, if this number is zero.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Short.takeHighestOneBit(): Short = (toInt() and 0xFFFF).takeHighestOneBit().toShort()
+
+/**
+ * Returns a number having a single bit set in the position of the least significant set bit of this [Short] number,
+ * or zero, if this number is zero.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+@kotlin.internal.InlineOnly
+public inline fun Short.takeLowestOneBit(): Short = toInt().takeLowestOneBit().toShort()
--- libraries/stdlib/jvm/test/numbers/NumbersJVMTest.kt
+++ libraries/stdlib/jvm/test/numbers/NumbersJVMTest.kt
@@ -23,4 +23,88 @@ class NumbersJVMTest {
         assertTrue(Float.NaN.toRawBits().let { Float.fromBits(it) }.isNaN())
     }
 
+
+    @UseExperimental(ExperimentalStdlibApi::class)
+    @Test
+    fun byteBits() {
+        fun test(value: Byte, oneBits: Int, leadingZeroes: Int, trailingZeroes: Int) {
+            assertEquals(oneBits, value.countOneBits())
+            assertEquals(leadingZeroes, value.countLeadingZeroBits())
+            assertEquals(trailingZeroes, value.countTrailingZeroBits())
+            val highestBit = if (leadingZeroes < Byte.SIZE_BITS) 1.shl(Byte.SIZE_BITS - leadingZeroes - 1).toByte() else 0
+            val lowestBit = if (trailingZeroes < Byte.SIZE_BITS) 1.shl(trailingZeroes).toByte() else 0
+            assertEquals(highestBit, value.takeHighestOneBit())
+            assertEquals(lowestBit, value.takeLowestOneBit())
+        }
+
+        test(0, 0, 8, 8)
+        test(1, 1, 7, 0)
+        test(2, 1, 6, 1)
+        test(0x44, 2, 1, 2)
+        test(0x80.toByte(), 1, 0, 7)
+        test(0xF0.toByte(), 4, 0, 4)
+    }
+    
+    @UseExperimental(ExperimentalStdlibApi::class)
+    @Test
+    fun shortBits() {
+        fun test(value: Short, oneBits: Int, leadingZeroes: Int, trailingZeroes: Int) {
+            assertEquals(oneBits, value.countOneBits())
+            assertEquals(leadingZeroes, value.countLeadingZeroBits())
+            assertEquals(trailingZeroes, value.countTrailingZeroBits())
+            val highestBit = if (leadingZeroes < Short.SIZE_BITS) 1.shl(Short.SIZE_BITS - leadingZeroes - 1).toShort() else 0
+            val lowestBit = if (trailingZeroes < Short.SIZE_BITS) 1.shl(trailingZeroes).toShort() else 0
+            assertEquals(highestBit, value.takeHighestOneBit())
+            assertEquals(lowestBit, value.takeLowestOneBit())
+        }
+
+        test(0, 0, 16, 16)
+        test(1, 1, 15, 0)
+        test(2, 1, 14, 1)
+        test(0xF2, 5, 8, 1)
+        test(0x8000.toShort(), 1, 0, 15)
+        test(0xF200.toShort(), 5, 0, 9)
+    }
+
+    @UseExperimental(ExperimentalStdlibApi::class)
+    @Test
+    fun intBits() {
+        fun test(value: Int, oneBits: Int, leadingZeroes: Int, trailingZeroes: Int) {
+            assertEquals(oneBits, value.countOneBits())
+            assertEquals(leadingZeroes, value.countLeadingZeroBits())
+            assertEquals(trailingZeroes, value.countTrailingZeroBits())
+            val highestBit = if (leadingZeroes < Int.SIZE_BITS) 1.shl(Int.SIZE_BITS - leadingZeroes - 1) else 0
+            val lowestBit = if (trailingZeroes < Int.SIZE_BITS) 1.shl(trailingZeroes) else 0
+            assertEquals(highestBit, value.takeHighestOneBit())
+            assertEquals(lowestBit, value.takeLowestOneBit())
+        }
+
+        test(0, 0, 32, 32)
+        test(1, 1, 31, 0)
+        test(2, 1, 30, 1)
+        test(0xF002, 5, 16, 1)
+        test(0xF00F0000.toInt(), 8, 0, 16)
+    }
+
+    @UseExperimental(ExperimentalStdlibApi::class)
+    @Test
+    fun longBits() {
+        fun test(value: Long, oneBits: Int, leadingZeroes: Int, trailingZeroes: Int) {
+            assertEquals(oneBits, value.countOneBits())
+            assertEquals(leadingZeroes, value.countLeadingZeroBits())
+            assertEquals(trailingZeroes, value.countTrailingZeroBits())
+            val highestBit = if (leadingZeroes < Long.SIZE_BITS) 1L.shl(Long.SIZE_BITS - leadingZeroes - 1).toLong() else 0
+            val lowestBit = if (trailingZeroes < Long.SIZE_BITS) 1L.shl(trailingZeroes).toLong() else 0
+            assertEquals(highestBit, value.takeHighestOneBit())
+            assertEquals(lowestBit, value.takeLowestOneBit())
+        }
+
+        test(0, 0, 64, 64)
+        test(1, 1, 63, 0)
+        test(2, 1, 62, 1)
+        test(0xF002, 5, 48, 1)
+        test(0xF00F0000L, 8, 32, 16)
+        test(0x1111_3333_EEEE_0000L, 4 + 8 + 12, 3, 17)
+    }
+
 }
