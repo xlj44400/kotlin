6b5fe30f2d97350170eb471a710851bc6e2d7dc4
Ting-Yuan Huang <laszio@google.com>
2019-05-24 23:54:57 -0700

    JVM_IR: generate default constructor
    Quoted from https://kotlinlang.org/docs/reference/classes.html
    
    "On the JVM, if all of the parameters of the primary constructor have
     default values, the compiler will generate an additional parameterless
     constructor which will use the default values. This makes it easier to
     use Kotlin with libraries such as Jackson or JPA that create class
     instances through parameterless constructors."

---
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/JvmDefaultConstructorLowering.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.backend.jvm.lower
+
+import org.jetbrains.kotlin.backend.common.ClassLoweringPass
+import org.jetbrains.kotlin.backend.common.ir.passTypeArgumentsFrom
+import org.jetbrains.kotlin.backend.common.phaser.makeIrFilePhase
+import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
+import org.jetbrains.kotlin.ir.builders.declarations.addConstructor
+import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.expressions.impl.IrBlockBodyImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrDelegatingConstructorCallImpl
+import org.jetbrains.kotlin.ir.util.constructors
+import org.jetbrains.kotlin.ir.util.hasDefaultValue
+
+internal val jvmDefaultConstructorPhase = makeIrFilePhase(
+    ::JvmDefaultConstructorLowering,
+    name = "JvmDefaultConstructor",
+    description = "Generate default constructors for Java"
+)
+
+// Quoted from https://kotlinlang.org/docs/reference/classes.html
+//
+// "On the JVM, if all of the parameters of the primary constructor have default values, the compiler will generate an additional
+//  parameterless constructor which will use the default values. This makes it easier to use Kotlin with libraries such as Jackson
+//  or JPA that create class instances through parameterless constructors."
+private class JvmDefaultConstructorLowering(val context: JvmBackendContext) : ClassLoweringPass {
+
+    override fun lower(irClass: IrClass) {
+        val primaryConstructor = irClass.constructors.firstOrNull { it.isPrimary } ?: return
+
+        if (!primaryConstructor.valueParameters.all { it.hasDefaultValue() })
+            return
+
+        // Skip if the default constructor is already defined by user.
+        if (irClass.constructors.any { it.valueParameters.isEmpty() })
+            return
+
+        irClass.addConstructor().apply {
+            body = IrBlockBodyImpl(
+                primaryConstructor.startOffset,
+                primaryConstructor.endOffset,
+                listOf(
+                    IrDelegatingConstructorCallImpl(
+                        primaryConstructor.startOffset,
+                        primaryConstructor.endOffset,
+                        primaryConstructor.returnType,
+                        primaryConstructor.symbol
+                    ).apply {
+                        passTypeArgumentsFrom(irClass)
+                        passTypeArgumentsFrom(primaryConstructor, irClass.typeParameters.size)
+                    }
+                )
+            )
+        }
+    }
+}
