dca77632bd56ce1d1dc500311a89e1502c5ab267
Mads Ager <ager@google.com>
2019-06-03 15:12:00 +0200

    JVM_IR: Maintain annotations in SAM conversion.
    Copy method and parameter annotations to the generated SAM
    implementation method.

--- compiler/testData/codegen/box/annotations/annotatedLambda/samFunExpression.kt
+++ compiler/testData/codegen/box/annotations/annotatedLambda/samFunExpression.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND: JVM_IR
 // TARGET_BACKEND: JVM
 
 // WITH_RUNTIME
@@ -35,15 +34,28 @@ fun testMethod(method: Method, name: String) {
     }
 }
 
-fun testClass(clazz: Class<*>, name: String) {
+fun testMethodNoAnnotations(method: Method, name: String) {
+    assertEquals(0, method.getDeclaredAnnotations().size, "No method annotations expected `$name`")
+    for ((index, annotations) in method.getParameterAnnotations().withIndex()) {
+        assertEquals(0, annotations.size, "No parameter $index annotations expected `$name`")
+    }
+}
+
+
+fun testClass(clazz: Class<*>, name: String, methodTest: (Method, String) -> Unit) {
     val invokes = clazz.getDeclaredMethods().single() { !it.isBridge() }
-    testMethod(invokes, name)
+    methodTest(invokes, name)
+}
+
+@Ann("OK")
+fun annotatedFunction(@Ann("OK0") x: String, @Ann("OK1") y: String) {
 }
 
 fun box(): String {
-    testClass(Test.apply(@Ann("OK") fun(){}), "1")
-
-    testClass(Test.applyABC(@Ann("OK") fun(@Ann("OK0") x: String, @Ann("OK1") y: String){}), "2")
-
+    testClass(Test.apply(@Ann("OK") fun(){}), "1", ::testMethod)
+    testClass(Test.applyABC(@Ann("OK") fun(@Ann("OK0") x: String, @Ann("OK1") y: String){}), "2", ::testMethod)
+    testClass(Test.applyABC(::annotatedFunction), "3", ::testMethodNoAnnotations)
+    val reference = ::annotatedFunction
+    testClass(Test.applyABC(reference), "4", ::testMethodNoAnnotations)
     return "OK"
 }
