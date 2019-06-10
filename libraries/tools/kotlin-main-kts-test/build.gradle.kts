98daaf0003715ac0494e617e72b6830d348520ee
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2019-06-10 06:11:38 +0300

    Avoid indexing kotlin-main-kts runtime jar

--- libraries/tools/kotlin-main-kts-test/build.gradle.kts
+++ libraries/tools/kotlin-main-kts-test/build.gradle.kts
@@ -6,18 +6,14 @@ plugins {
 }
 
 dependencies {
-    if (!kotlinBuildProperties.isInJpsBuildIdeaSync) {
-        testCompile(projectRuntimeJar(":kotlin-main-kts")) // runtimeJar needed to for proper dependency on the jar with relocations
-    }
-
-    testCompile(project(":kotlin-scripting-jvm-host-embeddable"))
-    testCompile(commonDep("junit"))
     compileOnly("org.apache.ivy:ivy:2.4.0") // for jps/pill
-
-    if (kotlinBuildProperties.isInJpsBuildIdeaSync) {
-        testCompile(project(":kotlin-scripting-jvm-host"))
-        testCompile(project(":kotlin-main-kts"))
-    }
+    testCompile(commonDep("junit"))
+    testCompile(kotlinStdlib())
+    testRuntime(project(":kotlin-reflect"))
+    testCompileOnly(project(":kotlin-main-kts"))
+    testCompileOnly(project(":kotlin-scripting-jvm-host"))
+    testRuntimeOnly(projectRuntimeJar(":kotlin-main-kts"))
+    testRuntimeOnly(project(":kotlin-scripting-jvm-host-embeddable"))
 }
 
 sourceSets {
