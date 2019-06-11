e4ea06ca83941e8b3b9826467cc4eed9c77054ac
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2019-06-12 02:52:49 +0300

    ~ incremental instrumentation wip

--- gradle/javaInstrumentation.gradle.kts
+++ gradle/javaInstrumentation.gradle.kts
@@ -3,6 +3,13 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.attribute.FileTime
+
 allprojects {
     afterEvaluate {
         configureJavaInstrumentation()
@@ -12,6 +19,23 @@ allprojects {
 // Hide window of instrumentation tasks
 val headlessOldValue: String? = System.setProperty("java.awt.headless", "true")
 logger.info("Setting java.awt.headless=true, old value was $headlessOldValue")
+
+data class BasicFingerprint(val lastModified: FileTime, val size: Long) {
+    constructor(attrs: BasicFileAttributes) : this(attrs.lastModifiedTime(), attrs.size())
+}
+
+fun File.visitClasses(visitClassFile: (path: Path, attrs: BasicFileAttributes) -> Unit) {
+    val classFileVisitor = object : SimpleFileVisitor<Path>() {
+        override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+            if (file.toString().endsWith(".class")) {
+                visitClassFile(file, attrs)
+            }
+            return FileVisitResult.CONTINUE
+        }
+    }
+
+    Files.walkFileTree(toPath(), classFileVisitor)
+}
 
 /**
  *  Configures instrumentation for all JavaCompile tasks in project
@@ -26,14 +50,55 @@ fun Project.configureJavaInstrumentation() {
         }
 
         tasks.withType<JavaCompile> {
+            val oldFingerprints = HashMap<Path, BasicFingerprint>()
+            doFirst {
+                if (options.isIncremental) {
+                    destinationDir.visitClasses { path, attrs ->
+                        oldFingerprints[path] = BasicFingerprint(attrs)
+                    }
+                }
+            }
             doLast {
-                instrumentClasses(javaInstrumentator.asPath)
+                if (oldFingerprints.isEmpty()) {
+                    // instrument non incrementally in-place
+                    instrumentClasses(javaInstrumentator.asPath, destinationDir, classpath)
+                    println("Instrumented all classes")
+                } else {
+                    //instrument incrementally
+                    val classesDir = destinationDir
+                    val instrumentationDir = project.buildDir.resolve("nullInstr_$name")
+                    instrumentationDir.deleteRecursively()
+                    instrumentationDir.mkdirs()
+
+                    val instrumentedToOriginal = HashMap<Path, Path>()
+                    val sourceDir = classesDir.toPath()
+                    val targetDir = instrumentationDir.toPath()
+
+                    classesDir.visitClasses { path, attrs ->
+                        val oldPrint = oldFingerprints[path]
+                        val newPrint = BasicFingerprint(attrs)
+                        if (oldPrint == null || oldPrint != newPrint) {
+                            val targetPath = targetDir.resolve(sourceDir.relativize(path))
+                            Files.createDirectories(targetPath.parent)
+                            Files.move(path, targetPath)
+                            instrumentedToOriginal[targetPath] = path
+                        }
+                    }
+
+                    instrumentClasses(javaInstrumentator.asPath, instrumentationDir, project.files(destinationDir) + classpath)
+
+                    for ((instrumented, original) in instrumentedToOriginal) {
+                        Files.move(instrumented, original)
+                    }
+
+                    println("Instrumented ${instrumentedToOriginal.size} classes")
+                }
             }
         }
     }
 }
 
-fun JavaCompile.instrumentClasses(instrumentatorClasspath: String) {
+fun JavaCompile.instrumentClasses(instrumentatorClasspath: String, instrumenterDir: File, myClasspath: FileCollection) {
     ant.withGroovyBuilder {
         "taskdef"(
             "name" to "instrumentIdeaExtensions",
@@ -51,8 +116,8 @@ fun JavaCompile.instrumentClasses(instrumentatorClasspath: String) {
         javaSourceDirectories.forEach { directory ->
             "instrumentIdeaExtensions"(
                 "srcdir" to directory,
-                "destdir" to destinationDir,
-                "classpath" to classpath.asPath,
+                "destdir" to instrumenterDir,
+                "classpath" to myClasspath.asPath,
                 "includeantruntime" to false,
                 "instrumentNotNull" to true
             )
